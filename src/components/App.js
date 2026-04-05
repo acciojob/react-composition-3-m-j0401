f93bd54cb8653bc2d377d8a80fd04f6d2fc8cd33
Mehul Jain <mehul.jain2206@gmail.com>
2026-04-05 15:26:18 +0530

    data3

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
        <Tooltip className="tooltip" text1="This is a tooltip" children1="Hover over me" onMouseover={handlechange}/>
-       <Tooltip text2="This is a another tooltip" children2="Hover over me to see another tooltip"></Tooltip>
+       <Tooltip className="tooltip" text2="This is a another tooltip" children2="Hover over me to see another tooltip"></Tooltip>
     </div>
   )
 }
