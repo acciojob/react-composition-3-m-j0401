2112b59d43e198c0aa9d50942de929cdc4e13cb1
Mehul Jain <mehul.jain2206@gmail.com>
2026-04-05 15:37:27 +0530

    data4

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,10 @@ import './../styles/App.css';
 import Tooltip from "./Tooltip";
 
 const App = () => {
-  let[data,setData]=useState("");
-  function handlechange(e){
-    setData(e.target.text)
-  }
+ 
   return (
     <div>
-       <Tooltip className="tooltip" text1="This is a tooltip" children1="Hover over me" onMouseover={handlechange}/>
+       <Tooltip className="tooltip" text1="This is a tooltip" children1="Hover over me"/>
        <Tooltip className="tooltip" text2="This is a another tooltip" children2="Hover over me to see another tooltip"></Tooltip>
     </div>
   )
